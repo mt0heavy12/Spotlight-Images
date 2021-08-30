d4627345d216e2acf1ad3dd2a8147654f88b5db5
MrHeavy1 <89767587+MrHeavy1@users.noreply.github.com>
2021-08-30 13:31:03 +0600

    Delete Spotlight Image Collection Script.bat

--- Spotlight Image Collection Script.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-mkdir %LocalAppData%\Spotlight
-cd %LocalAppData%\Spotlight
-del *.* /f /q
-cd %LocalAppData%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
-copy *.* %LocalAppData%\Spotlight
-cd %LocalAppData%\Spotlight
-ren * *.jpg
-explorer %LocalAppData%\Spotlight
