c22ff056b94332b008f68cd0eda8bf29e425771d
mt0heavy12 <77685252+mt0heavy12@users.noreply.github.com>
2022-09-06 10:41:16 +0600

    Updated to ask if the files existing in the folder should be deleted

--- SpotlightCollection.bat
+++ SpotlightCollection.bat
@@ -1,12 +1,28 @@
 @echo off
 mkdir %LocalAppData%\WindowsSpotlight_WALLPAPER
-cd %LocalAppData%\WindowsSpotlight_WALLPAPER
-del *.* /f /q
+cls
+CHOICE /C 34 /M "Delete everything in the Spotlight image collection folder [3=Yes,4=No]"
+
+IF ERRORLEVEL 4 GOTO Def
+IF ERRORLEVEL 3 GOTO Del
+
+:Def
 cd %LocalAppData%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
 copy *.* %LocalAppData%\WindowsSpotlight_WALLPAPER
 cd %LocalAppData%\WindowsSpotlight_WALLPAPER
 ren * *.jpg
 cls
+
+:Del
+cd %LocalAppData%\WindowsSpotlight_WALLPAPER
+del *.* /f /q
+rmdir /s /q
+cd %LocalAppData%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
+copy *.* %LocalAppData%\WindowsSpotlight_WALLPAPER
+cd %LocalAppData%\WindowsSpotlight_WALLPAPER
+ren * *.jpg
+cls
+
 CHOICE /C 12 /M "Open the folder with images [2=No,1=Yes]"
 
 IF ERRORLEVEL 2 GOTO No
